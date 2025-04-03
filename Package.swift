018c5f9c4d74c4661b3abf5620ff611450ff0549
Anilkumar <32846534+Anilkumar18@users.noreply.github.com>
2025-04-03 14:26:02 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,20 +1,20 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
-    name: "ToneFramework",
+    name: "ToneListen",
     platforms: [
         .iOS(.v13)
     ],
     products: [
         .library(
-            name: "ToneFramework",
-            targets: ["ToneFramework"]
-        ),
+            name: "ToneListen",
+            targets: ["ToneListen"]
+        )
     ],
     targets: [
         .binaryTarget(
-            name: "ToneFramework",
+            name: "ToneListen",
             path: "Sources/ToneListen.xcframework"
         )
     ]
